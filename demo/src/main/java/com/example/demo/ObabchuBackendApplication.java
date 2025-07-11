28b2fde5ce1f026ca62d2dce9945f272e483e589
Shuung <qmoy2510@gmail.com>
2025-07-12 00:42:08 +0900

    프로젝트 생성

---
+++ demo/src/main/java/com/example/demo/ObabchuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObabchuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObabchuBackendApplication.class, args);
+	}
+
+}
